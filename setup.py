8b4531db2b4e0ccd3522d6f310e7c2b106b03809
Zach Reichert <zach.reichert@rackspace.com>
2018-04-25 07:33:31 -0700

    ASC-397 update release processs
    - uses bumpversion
    - adds new make tasks to realese & tag

--- setup.py
+++ setup.py
@@ -1,29 +1,17 @@
-# -*- encoding:utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import codecs
-import re
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
-
-def get_version(filename):
-    with codecs.open(filename, 'r', 'utf-8') as fp:
-        contents = fp.read()
-    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
-
-
-version = get_version('flake8_pytest_mark.py')
-
-with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 setup(
     name='flake8-pytest-mark',
-    version=version,
+    version='0.1.0',
     description="A flake8 plugin that helps check the presence of a PyTest mark",
     long_description=readme + '\n\n' + history,
     author="Zach Reichert",
@@ -40,14 +28,14 @@ setup(
         'flake8!=3.2.0',
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    license="ISCL",
+    license="Apache Software License 2.0",
     zip_safe=False,
     keywords='flake8-pytest-mark',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Framework :: Flake8',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
--- tests/test_no_configuration.py
+++ tests/test_no_configuration.py
@@ -1,4 +1,4 @@
-# -*- encoding:utf-8 -*-
+# -*- coding: utf-8 -*-
 import pytest
 
 # args to only use checks that raise an 'M' prefixed error
