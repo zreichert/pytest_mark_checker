f8fa96bcee623af7b292bf5dd9bf0e3783f6a53d
Zach Reichert <zach.reichert@rackspace.com>
2018-05-24 08:45:35 -0700

    Bump version: 0.1.1 → 0.2.0

--- flake8_pytest_mark/__init__.py
+++ flake8_pytest_mark/__init__.py
@@ -5,7 +5,7 @@ from flake8_pytest_mark import rules
 
 __author__ = 'Zach Reichert'
 __email__ = 'zach.reichert@rackspace.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 
 class MarkChecker(object):
